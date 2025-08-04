d993302af7723a71dc9223695b4d80c861e55a7d
Davi Komura de Castro <davi.komura@gmail.com>
2025-08-04 09:18:53 +0100

    config vercel

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -2,7 +2,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: 'https://differentiationbee-backend.onrender.com',
+    // baseURL: 'http://localhost:8000',
     headers: {
         'Content-Type': 'application/json',
     }
